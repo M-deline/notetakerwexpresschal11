4d5042264093833db67d3f13ff7926fb6cbcfde2
Madeline K <madkovanda@gmail.com>
2023-11-23 09:52:21 -0700

    had 2 make new repo, add all work done

---
+++ Develop/server.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const uniqid = require("uniqid");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+app.get("/api/notes", function (req, res) {
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else if (data) {
+      var jsonData = JSON.parse(data);
+      res.json(jsonData);
+    } else {
+      console.error('Data is undefined');
+    }
+  })
+});
+const readThenAppendToJson = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeNewNoteToJson(file, parsedData);
+    }
+  });
+};
+const writeNewNoteToJson = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title: title,
+      text: text,
+      id: uniqid(),
+    };
+
+    readThenAppendToJson(newNote, "./db/db.json");
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json("Error in posting new note");
+  }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  let id = req.params.id;
+  let parsedData;
+  fs.readFile("/db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      parsedData = JSON.parse(data);
+      const filterData = parsedData.filter((note) => note.id !== id);
+      writeNewNoteToJson("./db/db.json", filterData);
+    }
+  });
+  res.send(`Deleted note with ${req.params.id}`);
+});
+
+app.listen(PORT, () =>
+  console.log(`See http://localhost:${PORT} `)
+);
